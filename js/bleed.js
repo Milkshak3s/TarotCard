6273776d6cfdc0dbd338941fdfd22da12229ae8b
SkidMaster <his.lord.markus@gmail.com>
2013-07-10 16:52:04 -0700

    6a -- Beginning multi-btn JS; Cleaup

--- js/bleed.js
+++ js/bleed.js
@@ -1,10 +1,10 @@
 var card1 = '/img/aleph.png';
 //Broken
-var card2 = 'img/beth.png';
+    var card2 = 'img/beth.png';
 //Broken
-var card3 = 'img/gimel.png';
+    var card3 = 'img/gimel.png';
 //Broken
-var card4 = 'img/daleth.png'
+    var card4 = 'img/daleth.png'
 var card5 = 'img/heh.png';
 var card6 = 'img/vav.png'
 var card7 = 'img/zain.png';
@@ -16,70 +16,113 @@ var card12 = 'img/lamed.png';
 var card13 = 'img/mem.png';
 var card14 = 'img/nun.png';
 //Broken
-var card15 = 'img/samekh.png';
+    var card15 = 'img/samekh.png';
 var card16 = 'img/ayin.png';
 var card17 = 'img/peh.png';
 var card18 = 'img/tzaddi.png';
 var card19 = 'img/qoph.png'
 var card20 = 'img/resh.png';
 //Broken
-var card21 = 'img/shin.png';
+    var card21 = 'img/shin.png';
 var card22 = 'img/tav.png';
 
 var img = $('#img');
+var btn1 = $('#nameBtn1')
+var btn2 = $('#nameBtn2')
+var btn3 = $('#nameBtn3')
 var cardNum = 1;
-//Switch state -- card changer (passing)
+//Switch state -- card changer (bleed // unfinished)
 $('#changeImg').on('click', function () {
     switch (cardNum) {
         case 1:
             img.attr('src', card2)
             cardNum = 2;
+            btn1.html('OptionOne')
+            btn2.html('OptionTwo')
+            btn3.html('OptionThree')
             break;
 
         case 2:
             img.attr('src', card3)
             cardNum = 3;
+            btn1.html('Ox')
+            btn2.html('Serpent')
+            btn3.html('House')
             break;
 
         case 3:
             img.attr('src', card4)
             cardNum = 4;
+            btn1.html('Ox')
+            btn2.html('Serpent')
+            btn3.html('House')
             break;
 
         case 4:
             img.attr('src', card5)
             cardNum = 5;
+            btn1.html('Ox')
+            btn2.html('Serpent')
+            btn3.html('House')
             break;
 
         case 5:
             img.attr('src', card6)
             cardNum = 6;
+            btn1.html('Ox')
+            btn2.html('Serpent')
+            btn3.html('House')
             break;
 
         case 6:
             img.attr('src', card7)
             cardNum = 7;
+            btn1.html('Ox')
+            btn2.html('Serpent')
+            btn3.html('House')
             break;
 
         case 7:
             img.attr('src', card8)
             cardNum = 8;
+            btn1.html('Ox')
+            btn2.html('Serpent')
+            btn3.html('House')
             break;
 
         case 8:
             img.attr('src', card9)
             cardNum = 9;
+            btn1.html('Ox')
+            btn2.html('Serpent')
+            btn3.html('House')
             break;
 
         default:
             img.attr('src', card1)
             cardNum = 1;
+            btn1.html('Ox')
+            btn2.html('Serpent')
+            btn3.html('House')
             break;
 
     }
 })
 
-//Muti-buttons (bleed)
+//Muti-buttons [on-hover] (non-func)
+$('#nameBtn1').on('hover', function () {
+    
+})
+
+$('#nameBtn2').on('hover', function () {
+    
+})
+
+$('#nameBtn3').on('hover', function () {
+    
+})
+
+//Muti-buttons [on-click] (non-func)
 $('#nameBtn1').on('click', function () {
     
 })
