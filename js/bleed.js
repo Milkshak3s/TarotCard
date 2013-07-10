116c2c16be78e8fc2149f5764cea13cdbc54df9d
Kayla Block <kblock111@gmail.com>
2013-07-09 19:28:36 -0700

    4a
    vars

--- js/bleed.js
+++ js/bleed.js
@@ -1,4 +1,25 @@
-var card1 = '/img/ayin.png';
+var card1 = '/img/aleph.png';
+// var card2 = 'img/beth.png';
+//var card3 = 'img/gimel.png';
+// var card4 = 'img/daleth.png'
+var card5 = 'img/heh.png';
+var card6 = 'img/vav.png'
+var card7 = 'img/zain.png';
+var card8 = 'img/cheth.png';
+var card9 = 'img/tet.png';
+var card10 = 'img/yod.png';
+var card11 = 'img/kaph.png';
+var card12 = 'img/lamed.png';
+var card13 = 'img/mem.png';
+var card14 = 'img/nun.png';
+//var card15 = 'img/samekh.png';
+var card16 = 'img/ayin.png';
+var card17 = 'img/peh.png';
+var card18 = 'img/tzaddi.png';
+var card19 = 'img/qoph.png'
+var card20 = 'img/resh.png';
+//var card21 = 'img/shin.png';
+var card22 = 'img/tav.png';
 
 $('#changeImg').on('click', function () {
     $('#img').attr('src', card1);
