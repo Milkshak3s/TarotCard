6801903e3b5bc63e9fe2408e334f7dfd30e38f9a
Chris <pop99910@gmail.com>
2013-07-09 19:29:51 -0700

    4b -- Bleeding switch

--- js/bleed.js
+++ js/bleed.js
@@ -21,6 +21,46 @@ var card20 = 'img/resh.png';
 //var card21 = 'img/shin.png';
 var card22 = 'img/tav.png';
 
+var img = $('#img');
+var cardNum = 1;
 $('#changeImg').on('click', function () {
-    $('#img').attr('src', card1);
+    if (cardNum == 1) {
+        $('#img').attr('src', card2);
+        cardNum = 2;
+    }
+    else {
+        $('#img').attr('src', card1)
+        cardNum = 1;
+    }
 });
+
+//Switch statement (bleed)
+$('#changeImg').on('click', function () {
+    switch (cardNum) {
+        case 1:
+            img.attr('src', card2)
+            cardNum = 2;
+            break;
+
+        case 2:
+            img.attr('src', card3)
+            cardNum = 3;
+            break;
+
+        case 3:
+            img.attr('src', card4)
+            cardNum = 4;
+            break;
+
+        case 4:
+            img.attr('src', card5)
+            cardNum = 5;
+            break;
+
+        default:
+            img.attr('src', card1)
+            cardNum = 1;
+            break;
+
+    }
+})
